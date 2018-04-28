093fa8cfeee8ef19e06d337f71f26f1b2ad61f3a
anonymous-authors-recsys <37581717+anonymous-authors-recsys@users.noreply.github.com>
2018-04-28 15:10:34 +0200

    Add music_2 data download

--- download-script.sh
+++ download-script.sh
@@ -17,3 +17,8 @@ filename="data/music_1.npy"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
+fileid="1nzlfjKcr9vYjpon24svXmGlTiRLxEfQf"
+filename="data/music_2.npy"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
